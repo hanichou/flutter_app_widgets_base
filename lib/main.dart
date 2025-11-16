e0fe9d469ab9abf67d1a57f3d36428416b2b18fd
Hani <haniaghilas20@gmail.com>
2025-11-16 03:55:25 -0500

    Étape 4 : ajout du Stack superposé

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class MainApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
+              // Étape 2
               Text('Hello World!'),
               Text('Deuxième texte'),
 
-              
+              // Étape 3 : Container stylisé
               Container(
                 margin: EdgeInsets.all(20),
                 padding: EdgeInsets.all(16),
@@ -29,8 +30,39 @@ class MainApp extends StatelessWidget {
                 ),
               ),
 
-              
-              Text("je m'appelle Hani"),
+              // Texte ajouté pour l'étape 3
+              Text("Texte ajouté pour l'étape 3"),
+
+              SizedBox(height: 20),
+
+              // ÉTAPE 4 : STACK
+              Stack(
+                alignment: Alignment.topRight, // modifié comme demandé
+                children: [
+                  // Carré de fond
+                  Container(
+                    width: 200,
+                    height: 200,
+                    color: Colors.blueAccent,
+                  ),
+
+                  // Icône superposée
+                  Icon(
+                    Icons.star,
+                    size: 100,
+                    color: Colors.amber,
+                  ),
+
+                  // Texte superposé
+                  Text(
+                    "Superposé !",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
